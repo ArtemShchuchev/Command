8cb8b9c0e092fb41660bc07dc7a3a2bcf41ed234
ArtemShchuchev <shuaa@rambler.ru>
2023-04-22 18:52:25 +0700

    Add files

---
+++ Commands/Command.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+class LogCommand
+{
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿//#include <cassert> // для assert()
+#include "SecondaryFunction.h"
+#include "Command.h"
+
+/*
+Задание 1. Паттерн «Команда»
+Пишем команды для системы логирования.
+
+В задании нужно реализовать наследников класса:
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+таким образом, чтобы один наследник печатал сообщение message в
+консоль, а другой умел печатать сообщение в файл, по указанному пути.
+
+Далее нужно реализовать функцию: oid print(LogCommand&);, которая
+будет выполнять команды.
+*/
+
+int main(int argc, char** argv)
+{
+	printHeader(L"Задание 1. Паттерн «Команда»");
+    
+    
+	
+	std::wcout << "\n";
+	return 0;
+}
